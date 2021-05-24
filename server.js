b1edc240d7b9c5b2612cc6fb0dc6d736d5b6367b
Bugz296 <jcsbugnay@gmail.com>
2021-05-24 12:13:32 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,43 @@
+/**
+ * Require Modules
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+app.set('views', __dirname+'/views');
+app.set('view engine', 'ejs');
+
+const server = app.listen(8000, function(){
+    console.log("Listening to Port 8000");
+});
+const io = require('socket.io')(server);
+
+/* Socket Event Listener */
+let count = 0;
+io.on('connection', function(socket){
+    /* Whenever a client establishes a connection, server emits updated count */
+    socket.emit('updated_count', {data: count});
+    
+    socket.on('update_count', function(req){
+        count++;
+        io.emit('updated_count', {data: count});
+    });
+
+    socket.on('reset', function(req){
+        count = 0;
+        io.emit('updated_count', {data: count});
+    });
+});
+/**
+ * Routes
+ */
+app.get('/', function(req, res){
+    res.render('index', {user_data: "Hey"});
+});
+
+app.post('/users', function(req, res){
+    res.render('users', {user: req.body});
+});
